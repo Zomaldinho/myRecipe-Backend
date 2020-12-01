7986100642b6ecf87c7a5dd48f42c10fca9ebfca
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2020-12-01 12:14:23 +0200

    solving public access for images

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const cors = require('cors')
-require('./db/connect')
-const recipesRoutes = require('./routes/recipesRoutes')
+const cors = require('cors');
+const path = require('path');
+require('./db/connect');
+const recipesRoutes = require('./routes/recipesRoutes');
 
 const app = express();
 app.use(cors());
-app.use(express.json())
-app.use('/uploads',express.static('uploads'))
-app.use(recipesRoutes)
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(recipesRoutes);
 
-port = process.env.PORT || 5000
+port = process.env.PORT || 5000;
 
-app.listen(port, ()=> console.log(`App is running on port ${port}`))
+app.listen(port, () => console.log(`App is running on port ${port}`));
